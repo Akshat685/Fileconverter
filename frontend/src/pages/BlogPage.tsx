13843648cb054193e5399969647b7044e8f4720f
akshat <akshatshettigar2001@gmail.com>
2025-07-16 18:57:15 +0530

    Update BlogPage.tsx

--- frontend/src/pages/BlogPage.tsx
+++ frontend/src/pages/BlogPage.tsx
@@ -1,7 +1,7 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Blogs from '../components/Blogs'
-export default function AboutPage() {
+export default function BlogPage() {
     return (
         <div className="min-h-screen bg-white text-black">
             <Navbar />
